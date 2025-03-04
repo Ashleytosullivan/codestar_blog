1537e88c047c692a2a7fcba60be473a729fce897
Ashleytosullivan <ashleytosullivan@gmail.com>
2025-03-04 11:34:10 +0000

    create superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_x7sq+x@qifhd8dz(u515!aj9tvyz5za!p!(-3=j_5ig66y_^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 

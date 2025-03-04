ebbb49f2c246cec20c3c6dcfa30a618bfa06bd08
Ashleytosullivan <ashleytosullivan@gmail.com>
2025-03-04 15:56:50 +0000

    add html and css

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('summernote/', include('django_summernote.urls')),
+
 ]
